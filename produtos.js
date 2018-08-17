eb29bf11d8285989ef56567965510870228c7bd8
Victor Almeida De Freitas <victor.freitas08@hotmail.com>
2018-08-16 21:56:47 -0300

    primeiro codigo node

---
+++ produtos.js
@@ -0,0 +1,13 @@
+var http = require('http');//serve para carregar uma lib desejada
+var porta = 3000;
+var ip = "localhost";
+
+var server = http.createServer(function(req, res) { // req serve para pegar a requisição e o res para a resposta ou seja callback
+    console.log("Recebendo request");
+    res.writeHead(    200, {'Content-Type': 'text/html'});
+    res.end('<html><body>Request recebido!</body></html>');
+});
+
+server.listen(porta, ip);
+
+console.log("Server running at http://" + ip + ":" + porta + "/");
