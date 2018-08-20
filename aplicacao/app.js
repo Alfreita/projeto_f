efe6e29a6cd02bfcabd956595feb692634001e0a
tc.vfreitas <tc.vfreitas@padtec.com.br>
2018-08-20 16:42:36 -0300

    mais uma refatoração

--- aplicacao/app.js
+++ aplicacao/app.js
@@ -1,7 +1,5 @@
 var app = require ('./config/express')();
 
-app.set('view engine','ejs');
-
 app.get('/index', function(req,res){ // requisição e resposta
   console.log("Atendendo requisicao");
   res.render("html/index");
