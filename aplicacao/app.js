deb880881e696d322867a4f10bc47bd49c58752b
tc.vfreitas <tc.vfreitas@padtec.com.br>
2018-08-20 16:27:54 -0300

    log de request

--- aplicacao/app.js
+++ aplicacao/app.js
@@ -4,6 +4,7 @@ var app = express();
 app.set('view engine','ejs');
 
 app.get('/index', function(req,res){ // requisição e resposta
+  console.log("Atendendo requisicao");
   res.render("html/index");
 });
 
