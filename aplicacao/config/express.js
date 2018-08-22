17b5316a1407ea5b6da924824b0ad1bdaee61399
Victor Almeida De Freitas <victor.freitas08@hotmail.com>
2018-08-22 18:46:24 -0300

    ta subindo umas paginas, entretanto não é do melhor jeito

--- aplicacao/config/express.js
+++ aplicacao/config/express.js
@@ -1,6 +1,6 @@
 var app = require('express')();
 app.set('view engine', 'ejs');
 
-module. exports = function() {
+module.exports = function() {
     return app;
 };
--- aplicacao/routes/produtos.js
+++ aplicacao/routes/produtos.js
@@ -1,6 +1,21 @@
+
+
 module.exports = function(app){
-    app.get('/index', function(req,res){
-        console.log("GET de index");
-        res.render("html/index");
+    app.get('/produtos', function(req,res){
+        var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  dbo.collection("customers").find({}).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    res.render('html/produtos',{produtos:result});
+    db.close();
+  });
+}); 
+
     });
-}
+                
+};
