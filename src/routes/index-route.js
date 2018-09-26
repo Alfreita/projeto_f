56205b10a2e9df190879a4dede1911d37be7ea47
Victor Almeida De Freitas <victor.freitas08@hotmail.com>
2018-09-25 21:20:30 -0300

    agr vai caralho

---
+++ src/routes/index-route.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req,res,next)=>{
+    res.status(200).send({
+        title:"Node api",
+        version:"0.0.1"
+    });
+});
+
+router.get('/index',(req,res,next)=>{
+    res.render("index.ejs");
+});
+
+module.exports = router;
