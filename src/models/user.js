4ce57c1572ee44c2ae72783e354584bdcb84d945
Victor Almeida De Freitas <victor.freitas08@hotmail.com>
2018-10-02 20:03:53 -0300

    culpa do lusca que fez eu errar o projeto

---
+++ src/models/user.js
@@ -0,0 +1,88 @@
+'use strict';
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Schema = new Schema({
+   usuario:{
+       type:String,
+       required:true,
+       trim:true
+   },
+	cpf:{
+        type:Number,
+        require:true,
+        trim:true,
+        unique:true,
+        require:true
+    },
+	cnpj:{
+        type:Number,
+        trim:true,
+        unique:true
+
+    },
+	rg:{
+        type:Number,
+        required:true,
+        unique:true
+        
+    },
+	nome:{
+        type:String,
+        required:true,
+    },
+	email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+	telefones:[ {
+        type:Number,
+        require:true,
+        unique:true
+    }],
+	endereco: {    
+         cep:{
+            type:Number,
+            require:true
+            },
+			cidade:{
+                type:String
+            },
+			rua:{
+                
+            }
+					"bairro":,
+					"numero":,
+					"complemento"
+				},
+	"cartoes": [ {
+				"bandeira":,
+				"nomeNoCartao":,
+				"numero":,
+				"dataValidade":
+				} ],
+
+	"carrinhoDeCompras": {  "produtos": [],
+							"valorTotal":
+						 },
+	"compras": [ { "produtos": [],
+					"valorTotal":,
+					"qtdParcelas":,
+					"numeroPedido":,
+					"valorFrete"
+				} ],
+}
+
+pedido {
+	"numeroPedido":,
+	"enderecoEntrega": { "cep":,
+					     "cidade":,
+					     "rua":,
+					     "bairro":,
+					     "numero":,
+					     "complemento"
+						},
+	"produtos": []					
+
+}
+});
